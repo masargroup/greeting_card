141b9eea8033ee801c5c35dad1d82e8d798da6a9
MAHA <mem880.a@gmail.com>
2025-03-27 10:10:23 +0300

    Fix: deploy error

--- public/cards/images.js
+++ public/cards/images.js
@@ -1,6 +1,6 @@
-import card1 from 'cards/Card_01.png';
-import card2 from 'cards/Card_02.png';
-import card3 from 'cards/Card_03.png';
-import card4 from 'cards/Card_04.png';
+import card1 from './Card_01.png';
+import card2 from './Card_02.png';
+import card3 from './Card_03.png';
+import card4 from './Card_04.png';
 
 export { card1, card2, card3, card4 };
