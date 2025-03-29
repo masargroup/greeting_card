2079aab80e907750c6328744d9f731b35ad854ac
MAHA <mem880.a@gmail.com>
2025-03-29 09:34:02 +0300

    Fix: error build

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
