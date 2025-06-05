7e975a2980d968ca00d437c4444595f7eacac621
hibahdesu <hibahsindi@gmail.com>
2025-06-05 22:48:20 +0300

    Update: the image

--- public/cards/images.js
+++ public/cards/images.js
@@ -1,6 +1,7 @@
-import card1 from './Card_2_01.png';
+import card1 from './Card_v2_01.png';
 import card2 from './Card_2_02.png';
 import card3 from './Card_2_03.png';
 
 
-export { card1, card2, card3 };
+
+export { card1, card2, card3};
