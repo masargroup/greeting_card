a0703575b7fbf6144959e3c7af88d272316d0d3e
hibahdesu <hibahsindi@gmail.com>
2025-06-05 21:44:38 +0300

    Update: the photos names

--- public/cards/images.js
+++ public/cards/images.js
@@ -1,6 +1,6 @@
-import card1 from './Card_01.png';
-import card2 from './Card_02.png';
-import card3 from './Card_03.png';
+import card1 from './Card_2_01.png';
+import card2 from './Card_2_02.png';
+import card3 from './Card_2_03.png';
 
 
 export { card1, card2, card3 };
